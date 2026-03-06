bda53173d61e326a1f879a1385dd97650eddc0fa
razanbash <razan.oyoy123@gmail.com>
2026-03-06 15:13:24 +0300

    feat : enhance user registeration process with improved validation and error handling
    fix role array problem

--- server/src/models/auth.Model.js
+++ server/src/models/auth.Model.js
@@ -1,8 +1,24 @@
+// import { pool } from "../config/db.js";
+
+// export const createUser = async (name, email, hashed_password, role) => {
+//   const query = `insert into users (name, email , hashed_password, role)
+//     values ($1, $2, $3, $4) returning id ,name , email, role`;
+//   const result = await pool.query(query, [name, email, hashed_password, role]);
+//   return result.rows[0];
+// };
+
+
+
 import { pool } from "../config/db.js";
 
 export const createUser = async (name, email, hashed_password, role) => {
-  const query = `insert into users (name, email , hashed_password, role)
-    values ($1, $2, $3, $4, $5) returning userid ,name , email, role`;
+  const query = `
+    insert into users (name, email, hashed_password, role)
+    values ($1, $2, $3, $4)
+    returning id, name, email, role
+  `;
+
   const result = await pool.query(query, [name, email, hashed_password, role]);
+
   return result.rows[0];
 };
